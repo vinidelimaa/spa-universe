72844a10b4d5db21f62952064628687b11e47b59
Vinicius Lima <vinidelimaa@gmail.com>
2023-07-19 15:56:54 +1000

    style.css updated

---
+++ js-modules/index.js
@@ -0,0 +1,14 @@
+// import { bgChange } from "./bgChange.js"
+import { Router } from "./router.js"
+
+const router = new Router()
+router.add("/", "./pages/home.html")
+router.add("/universe", "./pages/universe.html")
+router.add("/explore", "./pages/explore.html")
+router.add(404, "./pages/404.html")
+
+router.handle()
+
+window.onpopstate = () => router.handle()
+// the popstate event is triggered when the user navigates backward or forward in the browser's history. By assigning the router.handle() function to window.onpopstate, we are defining that the handle() function of the router object will be called every time the popstate event is triggered
+window.route = (event) => router.route(event)
---
+++ js-modules/router.js
@@ -0,0 +1,54 @@
+export class Router {
+  routes = {}
+
+  add(routeName, page) {
+    this.routes[routeName] = page
+  }
+
+  route(event) {
+    event.preventDefault()
+    //called to prevent the default behavior of the browser, which is to reload the entire page when a link is clicked.
+
+    window.history.pushState({}, "", event.target.href)
+    //used to modify the browser's history state. This method allows changing the URL in the address bar without triggering a full page reload. It takes three arguments: the state object (empty in this case), the title (an empty string), and the new URL (obtained from the event.target.href property, which represents the clicked link's URL)
+
+    this.handle()
+  }
+
+  handle() {
+    const { pathname } = window.location
+    // same as: 'const pathname = window.location.pathname'
+
+    const route = this.routes[pathname] || this.routes[404]
+
+    fetch(route)
+      .then((data) => {
+        if (!data.ok) {
+          throw new Error(`HTTP error: ${data.status}`)
+        }
+        return data.text()
+      })
+      .then(
+        (html) => (document.querySelector("#app").innerHTML = html),
+        this.bgChange(pathname)
+      )
+  }
+
+  bgChange(pathname) {
+    const body = document.body
+    const cssChange = document.querySelector("#cssChange")
+
+    switch (pathname) {
+      case "/universe":
+        body.style.backgroundImage = "url(./assets/2-universe-background.png)"
+        break
+      case "/explore":
+        body.style.backgroundImage = "url(./assets/3-explore-background.png)"
+        break
+      default:
+        // Define the standart background for other pages
+        body.style.backgroundImage = "url(./assets/1-home-background.png)"
+        break
+    }
+  }
+}
